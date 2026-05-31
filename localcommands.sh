97521aa6ab5c23ff9dffc9e1a5339ac5e0eacaba
cibexon <66065705+cibexon@users.noreply.github.com>
2026-05-31 17:09:35 +0000

    uploaded video results

--- localcommands.sh
+++ localcommands.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+
+kubectl patch ocirepository releases -n flux-system --type merge -p '{"spec":{"ref":{"tag":"latest"}}}' && kubectl annotate ocirepository releases -n flux-system reconcile.fluxcd.io/requestedAt="$(date +%s)" --overwrite
+kubectl get ocirepository releases -n flux-system -w
+
 kubectl port-forward -n kagent svc/kagent-ui 8080:8080 &
+kubectl port-forward -n agentregistry svc/agentregistry-api 8085:8080 &
 
 #Api key for LLM
 kubectl create secret generic gemini-secret \
